fa92f78894e44beaf7fc7ba0e5d83390e784e7fc
shanvit7 <shanvit7@gmail.com>
2022-08-22 15:40:52 +0530

    Landing Page made

--- Anime-app/frontend/src/Landing.jsx
+++ Anime-app/frontend/src/Landing.jsx
@@ -1,22 +1,68 @@
 import { useQuery, gql } from '@apollo/client';
+import {
+  Center,
+  Button
+ } from '@chakra-ui/react';
+import {ArrowForwardIcon} from '@chakra-ui/icons';
+import { motion } from 'framer-motion';
+import './stylesheets/landing.scss';
 
-const GET_SERVER_RESPONSE = gql`query { greet }`;
-const GET_DATA = gql`query {getUsers{
-  id
-  name
-}}`;
+const GET_GREETING = gql`query { appName }`;
 
 const LandingPage=()=>{
 
-    const { loading, error, data } = useQuery(GET_DATA);
+    const { loading, error, data } = useQuery(GET_GREETING);
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error :</p>;
     return(
-        <>
-          <h1>This Page is for testing purpose</h1>
-          <h2>Welcome {data.getUsers[0].name}</h2>
-        </>
+        <div className='landing-page-wrapper'>
+
+          <Center>
+           <motion.h1
+           initial={{y:-1000}}
+           animate={{y:40}}    
+          >
+          {data.appName}
+           </motion.h1>
+          </Center>
+
+          <Center>
+            <motion.p 
+            initial={{x:-1000,opacity:0}}
+            animate={{x:60,y:60,opacity:1}}
+            transition={{ ease: "easeOut", duration: 2 }}
+             >
+              We’re all otaku here
+            </motion.p>
+          </Center>
+
+          <Center>
+            <motion.h3
+            className='page-text'
+            initial={{opacity:0}}
+            animate={{y:100,opacity:1}}
+            transition={{duration:3}}
+            >
+              Over 1000 + animes listed
+            </motion.h3>
+          </Center>
+       
+          <Center>
+            <motion.div 
+             initial={{y:-1000}}
+             animate={{y:300}}
+            >
+            <Button
+            className='btn-mod'
+            >
+              <ArrowForwardIcon/>
+            </Button>
+            </motion.div>
+         </Center>
+    
+
+        </div>
     )
 }
 
--- Anime-app/frontend/src/index.js
+++ Anime-app/frontend/src/index.js
@@ -7,14 +7,28 @@ import {
   Route,
 } from "react-router-dom";
 import { InMemoryCache, ApolloClient, ApolloProvider} from '@apollo/client';
+import { ChakraProvider,extendTheme } from '@chakra-ui/react';
+
 const client = new ApolloClient({
     uri: 'http://localhost:8080/graphql',
     cache: new InMemoryCache(),
 });
 
+const theme = extendTheme({
+  styles:{
+      global:()=>({
+          body:{
+              bg:'#1A1428'
+          }
+      })
+  }
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
+    <ChakraProvider theme={theme}>
     <ApolloProvider client={client}>
     <BrowserRouter>
         <Routes>
@@ -22,5 +36,6 @@ root.render(
         </Routes>
     </BrowserRouter>,
     </ApolloProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
