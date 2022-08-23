76bfaf092471817a35ff0dbc3ed27a7e49204653
shanvit7 <shanvit7@gmail.com>
2022-08-24 02:27:38 +0530

    Frontend setup

--- Anime-app/frontend/pages/_app.js
+++ Anime-app/frontend/pages/_app.js
@@ -1,7 +1,23 @@
 import '../styles/globals.css';
+import { ChakraProvider,extendTheme } from '@chakra-ui/react';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const theme = extendTheme({
+  styles:{
+      global:()=>({
+          body:{
+              bg:'#1A1428'
+          }
+      })
+  }
+})
+
+
+function MyApp({ Component, pageProps }){
+  return( 
+  <ChakraProvider theme={theme}>
+    <Component {...pageProps} />
+  </ChakraProvider>
+  )
 }
 
 export default MyApp
--- Anime-app/frontend/pages/index.jsx
+++ Anime-app/frontend/pages/index.jsx
@@ -1,6 +1,34 @@
-import styles from './'
+import styles from './index.module.css';
+import { motion } from 'framer-motion';
+import {
+  Center,
+ } from '@chakra-ui/react';
+
 export default function LandingPage() {
   return (
-    <div>Welcome to AnimeGraph</div>
+    <>
+    <Center>
+       <motion.h1
+        className={styles.landingTitle}
+        initial={{y:-1000}}
+        animate={{y:80}}  
+        >
+         Welcome to AnimeGraph
+       </motion.h1>
+    </Center>
+
+    <Center>
+    <motion.p 
+      className={styles.landingText}
+      initial={{x:-1000,opacity:0}}
+      animate={{x:1,y:100,opacity:1}}
+      transition={{ ease: "easeOut", duration: 2 }}
+      >
+      We’re all otaku here
+    </motion.p>
+    </Center>
+
+
+    </>
   )
 }
